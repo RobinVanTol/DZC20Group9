394d496053492b5f63216babc18a2d6911254faa
RobinVanTol <robin01.rvt@gmail.com>
2020-12-08 00:21:37 +0100

    made initial imlementation with 1 "hacker" following you

---
+++ GameProject/Assets/hacker slap/scripts/cameraFollow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 offset;
+    public float smoothing = 0.25f;
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 desiredpos = target.position + offset;
+        Vector3 smoothpos = Vector3.Lerp(transform.position, desiredpos, smoothing);
+        transform.position = smoothpos;
+        transform.LookAt(target);
+    }
+}
---
+++ GameProject/Assets/hacker slap/scripts/hackermomevement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hackermomevement : MonoBehaviour
+{
+    public float moveSpeed = 3f;
+    public Rigidbody2D hacker;
+    public Animator animator;
+    public Vector2 movement;
+    public Transform player;
+    bool slapped;
+
+    void Start()
+    {
+        slapped = false;
+    }
+    void Update()
+    {
+        transform.LookAt(player);
+        movement = (transform.forward * moveSpeed * Time.deltaTime).normalized;
+        transform.eulerAngles = new Vector3(0, 0, 0);
+        if (!slapped)
+        {
+            animator.SetFloat("horizontal", movement.x / 2);
+            animator.SetFloat("vertical", movement.y);
+            animator.SetFloat("speed", movement.sqrMagnitude);
+            if (movement.x < 0)
+            {
+                transform.localScale = new Vector3(-1, 1, 1);
+            }
+            else
+            {
+                transform.localScale = new Vector3(1, 1, 1);
+            }
+        }
+    }
+
+    void FixedUpdate()
+    {
+        hacker.MovePosition(hacker.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
